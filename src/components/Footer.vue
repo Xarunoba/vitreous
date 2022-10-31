4e276033813efa2d00af157d9c1650377c58b1e0
Xarunoba <santosjeterjude@gmail.com>
2022-11-01 00:27:53 +0800

    Initial Commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex align-middle justify-center items-center text-sm border-t border-dotted pt-2 mt-2 border-white/50">
+        <span> Powered by </span>
+        <a href="https://vitejs.dev" class="mx-1" target="_blank">
+            <img src="/vite.svg" class="w-6 h-6" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" class="mx-1" target="_blank">
+            <img src="../assets/vue.svg" class="w-6 h-6" alt="Vue logo" />
+        </a>
+    </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <i-mdi-eye class="peer/logo mx-auto text-6xl transition duration-1000 ease-in-out hover:text-emerald-400" />
+        <a href="https://github.com/xarunoba" target="_blank"
+            class="peer/link text-white/[.87] font-bold text-lg transition duration-1000 ease-in-out peer-hover/logo:text-emerald-200 hover:text-cyan-500">Vitreous</a>
+        <p
+            class="text-sm transition duration-1000 ease-in-out peer-hover/logo:animate-pulse peer-hover/link:animate-pulse peer-hover/link:text-cyan-800">
+            Just another Vite
+            template.</p>
+    </div>
+</template>
